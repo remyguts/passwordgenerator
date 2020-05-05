4c067b3a1c05de97c7ef75dd9e09bf09e106c82f
Remy Guts <remyguts@gmail.com>
2020-05-04 18:11:29 -0700

    changed values var to const and split up the letters

--- index.js
+++ index.js
@@ -1,37 +1,99 @@
-function generate(){
+"use strict";
 
-//set password length
-let complexity = document.getElementById("slider").value;
+function generate() {
+  const values = {
+    uppers: [
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+      "G",
+      "H",
+      "I",
+      "J",
+      "K",
+      "L",
+      "M",
+      "N",
+      "O",
+      "P",
+      "Q",
+      "R",
+      "S",
+      "T",
+      "U",
+      "V",
+      "W",
+      "X",
+      "Y",
+      "Z",
+    ],
+    lowers: [
+      "a",
+      "b",
+      "c",
+      "d",
+      "e",
+      "f",
+      "g",
+      "h",
+      "i",
+      "j",
+      "k",
+      "l",
+      "m",
+      "n",
+      "o",
+      "p",
+      "q",
+      "r",
+      "s",
+      "t",
+      "u",
+      "v",
+      "w",
+      "x",
+      "y",
+      "z",
+    ],
+    digits: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+    special: [
+      "!",
+      "#",
+      "$",
+      "%",
+      "&",
+      "'",
+      "(",
+      ")",
+      "*",
+      "+",
+      ",",
+      "-",
+      ".",
+      "/",
+      ":",
+      ";",
+      "<",
+      "=",
+      ">",
+      "?",
+      "@",
+      "[",
+      "]",
+      "^",
+      "_",
+      "`",
+      "{",
+      "|",
+      "}",
+      "~",
+      "'",
+      '"',
+    ],
+  };
 
-let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()";
-//constant vs let.... which one
-let password = ""; 
-
-for (var i= 0; i <= complexity; i++) {
- //learned about complexity!!
-    password = password + values.charAt(Math.floor(Math.random * Math.floor(values.length - 1 )));
+  var password = "";
 }
-
-document.getElementById("display").value = password;
-
-document.getElementById("lastNums").innerHTML += password + "<br />";
-}
-document.getElementById("length").innerHTML = "Length: 25";
-
-document.getElementById("slider").oninput = function(){
-
-    if(document.getElementById("slider").value > 0){
-        document.getElementById("length").innerHTML = "Length: " + document.getElementById("slider").value;
-    }
-    else{
-        document.getElementById("length").innerHTML = "Length: 1";
- }
-}
-
-function copyPassword(){
-
-    document.getElementById("display").select();
-
-    document.execCommand("Copy");
-
-    alert("Password copied to clipboard");
