67764c5b6f93a7af51f643607b858dee730190fd
Remy Guts <remyguts@gmail.com>
2020-05-04 19:13:10 -0700

    fixed bugs and changed background

--- index.js
+++ index.js
@@ -1,162 +1,74 @@
-function generate() {
-  var values = {
-    uppers: [
-      "A",
-      "B",
-      "C",
-      "D",
-      "E",
-      "F",
-      "G",
-      "H",
-      "I",
-      "J",
-      "K",
-      "L",
-      "M",
-      "N",
-      "O",
-      "P",
-      "Q",
-      "R",
-      "S",
-      "T",
-      "U",
-      "V",
-      "W",
-      "X",
-      "Y",
-      "Z",
-    ],
-    lowers: [
-      "a",
-      "b",
-      "c",
-      "d",
-      "e",
-      "f",
-      "g",
-      "h",
-      "i",
-      "j",
-      "k",
-      "l",
-      "m",
-      "n",
-      "o",
-      "p",
-      "q",
-      "r",
-      "s",
-      "t",
-      "u",
-      "v",
-      "w",
-      "x",
-      "y",
-      "z",
-    ],
-    digits: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
-    special: [
-      "!",
-      "#",
-      "$",
-      "%",
-      "&",
-      "'",
-      "(",
-      ")",
-      "*",
-      "+",
-      ",",
-      "-",
-      ".",
-      "/",
-      ":",
-      ";",
-      "<",
-      "=",
-      ">",
-      "?",
-      "@",
-      "[",
-      "]",
-      "^",
-      "_",
-      "`",
-      "{",
-      "|",
-      "}",
-      "~",
-      "'",
-      '"',
-    ],
-  };
+let randomCharString = "";
+let charSet = [
+  {
+    char: "!#$%&'()*+,-./:;<=>?@[]^_`{|}~",
+    name: "Special Characters",
+    use: false,
+  },
+  {
+    char: "0123456789",
+    name: "Number Characters",
+    use: false,
+  },
+  {
+    char: "abcdefghijklmnopqrstuvwxyz",
+    name: "Lowercase Letters",
+    use: false,
+  },
+  {
+    char: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    name: "Uppercase Letters",
+    use: false,
+  },
+];
 
-  var password = "";
+//prompt user to pick a password length between 8 and 128 characters
 
-  var uppers = values["uppers"];
-  var lowers = values["lowers"];
-  var digits = values["digits"];
-  var special = values["special"];
+const generate = () => {
+  let passwordLength = parseInt(
+    prompt("Please select a password length between 8 and 128 characters.")
+  );
+  let passwordString = "";
 
-  var length = document.getElementById("range").value;
-
-  var boxLower = document.getElementById("lowercase");
-  var boxUpper = document.getElementById("uppercase");
-  var boxNum = document.getElementById("number");
-  var boxSym = document.getElementById("symbols");
-
-  var allValues = [];
-
-  if (boxUpper.checked === true) {
-    allValues = allValues.concat(uppers);
-  }
-
-  if (boxLower.checked === true) {
-    allValues = allValues.concat(lowers);
-  }
-
-  if (boxNum.checked === true) {
-    allValues = allValues.concat(digits);
-  }
-
-  if (boxSym.checked === true) {
-    allValues = allValues.concat(special);
-  }
-
-  if (length < 8 || length > 128) {
-    alert("The password must be between 8 and 128 characters !");
-  } else if (
-    boxUpper.checked === false &&
-    boxLower.checked === false &&
-    boxNum.checked === false &&
-    boxSym.checked === false
-  ) {
-    alert("You have to check at least one box !");
+  //check if password is long enough
+  if (passwordLength > 8 && passwordLength < 128) {
+    charSet.forEach((set) => {
+      const useChar = prompt(`Do you want to use ${set.name}?`).toLowerCase();
+      if (useChar === "yes" || useChar === "y") {
+        set.use = true;
+      }
+      if (set.use) {
+        randomCharString = randomCharString + set.char;
+      }
+    });
+    console.log(JSON.stringify(charSet));
   } else {
-    for (i = 0; i <= length; i++) {
-      password =
-        password + allValues[Math.floor(Math.random() * allValues.length)];
+    alert(
+      "Your password does not meet the requirements. Please refresh and try again."
+    );
+  }
+
+  //generate random password based on characters the user wants to use
+  if (randomCharString !== "") {
+    for (i = 1; i <= passwordLength; i++) {
+      passwordString =
+        passwordString +
+        randomCharString.charAt(
+          Math.floor(Math.random() * Math.floor(randomCharString.length - 1))
+        );
     }
+
+    document.getElementById("password").value = passwordString;
+  } else {
+    alert(
+      "You must use at least one kind of character. Please refresh and try again."
+    );
   }
+};
 
-  if (
-    boxUpper.checked === true ||
-    boxLower.checked === true ||
-    boxNum.checked === true ||
-    boxSym.checked === true
-  ) {
-    document.getElementById("box").value = password;
-  }
-
-  return password;
-}
-
-function copyClipboard() {
-  document.getElementById("box").select();
-
+const copy = () => {
+  const copyText = document.querySelector("#password");
+  copyText.select();
   document.execCommand("copy");
-
-  alert("Copied to Clipboard");
-}
+  alert("Your password has been copied to the clipboard.");
+};
