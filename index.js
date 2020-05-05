a9389ba56841c6cd0c4510715020078e087cc309
Remy Guts <remyguts@gmail.com>
2020-05-04 18:25:10 -0700

    set password prams and password return

--- index.js
+++ index.js
@@ -106,9 +106,41 @@ function generate() {
     let boxNum = document.getElementById("number");
     let boxSym = document.getElementById("symbols");
 
+    if (boxUpper.checked === true) {
+        allValues = allValues.concat(uppers)
+    }
 
+    if (boxLower.checked === true) {
+        allValues = allValues.concat(lowers)
+    }
 
+    if (boxNum.checked === true) {
+        allValues = allValues.concat(digits)
+    }
     
+    if (boxSym.checked === true) {
+        allValues = allValues.concat(special)
+    }
+
+    if (length < 8 || length > 128) {
+        alert("The password must be between 8 and 128 characters !")
+    } else if (boxUpper.checked === false && boxLower.checked === false && boxNum.checked === false && boxSym.checked === false) {
+        alert("You have to check at least one box !")
+    } else {
+        for (i = 0; i <= length; i++) {
+            password = password + allValues[Math.floor(Math.random() * allValues.length)]
+        }
+    } 
+    
+    if (boxUpper.checked === true || boxLower.checked === true || boxNum.checked === true || boxSym.checked === true) {
+        document.getElementById("box").value = password;
+    }
+
+    return password;
+ 
+}
+
+
 
     ]
 
